76f5b3690fab47526b5b572075dcd683a4697dc9
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-04-01 18:26:54 +0200

    try to expose port, no success

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import laravel from "vite-plugin-laravel";
 
 export default defineConfig({
     plugins: [svelte(), laravel()],
+    // https://github.com/iammati/vite-ddev/blob/master/app/frontend/vite.config.ts
+    // HMR server-port which is exposed by DDEV-Local in .ddev/docker-compose.hmr.yaml
+    server: {
+        port: 3000,
+
+        watch: {
+            usePolling: true
+        }
+    }
 });
+

368a49a58ca9e4752e2d808628835869cedabfcf
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-04-02 11:10:34 +0200

    add laravel vite preset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 
-import '../css/app.css';
+/*import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/inertia-svelte'
 
@@ -11,4 +11,32 @@ createInertiaApp({
 })
 
 // Test - watch doesn get triggered by vite?
-alert('hello vite 12345678910?!');
+alert('hello vite 12345678910?!');*/
+
+import { createInertiaApp } from '@inertiajs/inertia-svelte'
+
+// https://laravel-vite.dev/guide/extra-topics/inertia.html#example-implementation
+/**
+ * Imports the given page component from the page record.
+ */
+ function resolvePageComponent(name: string, pages: Record<string, any>) {
+    for (const path in pages) {
+      if (path.endsWith(`${name.replace('.', '/')}.svelte`)) {
+        return typeof pages[path] === 'function'
+          ? pages[path]()
+          : pages[path]
+      }
+    }
+
+    throw new Error(`Page not found: ${name}`)
+  }
+
+  // Creates the Inertia app, nothing fancy.
+  createInertiaApp({
+    resolve: (name) => resolvePageComponent(name, import.meta.glob('Pages/**/*.svelte')),
+    setup({ el, app, props, plugin }) {
+      createApp({ render: () => h(app, props) })
+        .use(plugin)
+        .mount(el)
+    },
+  })
